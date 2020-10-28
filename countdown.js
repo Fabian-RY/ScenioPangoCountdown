344baf59c5bc955a104f7eb3fccd23c8f194fba8
Fabián Robledo <fabianry97@gmail.com>
2020-10-28 23:00:47 +0100

    Updated to add seconds

--- countdown.js
+++ countdown.js
@@ -5,26 +5,30 @@ class program{
         this.day = day;
         this.hour = hour;
         this.min_ = min_;
-        this.factor = 1/3.3*Math.pow(10, 10);
+        this.factor = 1///3.3*Math.pow(10, 10);
     }
     
     
     
     calc_time(time, id){
-        var days_left = this.day*24*60- time.getDay()*24*60
-        var hours_left = this.hour*60 - time.getHours()*60
-        var minuts_left = this.min_ - time.getMinutes()
-        var minutes_now = time.getDay()*24*60 + time.getHours()*60 + time.getMinutes();
-        var minutes_program = this.day*24*60 + this.hour*60 + this.min_;
-        var minutes_left = minutes_program - minutes_now;
-        console.log(24*7*60, minutes_left, this.name);
-        if (minutes_left <0) {
-            minutes_left = 7*24*60 + minutes_left;
+        var days_left = this.day*24*60- time.getDay()*24*60;
+        var hours_left = this.hour*60 - time.getHours()*60;
+        var minuts_left = this.min_ - time.getMinutes();
+        var seconds_left = 60 - time.getSeconds();
+        var minutes_now = time.getDay()*24*60*60 + time.getHours()*60*60 + time.getMinutes()*60 + time.getSeconds();
+        var minutes_program = this.day*24*60*60 + this.hour*60*60 + this.min_*60;
+        var seconds_left = minutes_program - minutes_now;
+        //console.log(minutes_left, this.name);
+        if (seconds_left <0) {
+            seconds_left = 7*24*60*60 + seconds_left;
         }
-        console.log(minutes_left);
+        console.log(seconds_left);
         // Falta comprobar cuando se ha emitido ya el programa ese día
-        var time = minutes_left*this.factor;
-        document.getElementById(id).innerHTML = time.toExponential() + ' pangolines luz';
+        var seconds = seconds_left % 60; var minutes = Math.floor(seconds_left/60);
+        var mins = minutes % 60; var hours_ = Math.floor(minutes/60);
+        var hours = hours_ % 24; var days = Math.floor(hours_/24);
+        console.log(days, hours, mins, seconds)
+        document.getElementById(id).innerHTML = days + ' dias ' + hours + ' horas ' + mins + ' minutos ' + seconds + ' segundos restantes';
     }
 }
 
